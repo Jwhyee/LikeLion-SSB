b1789a43107e36012a5a9d7314c5082dc79ab013
Jwhyee <kawnsdud@naver.com>
2022-08-10 15:08:27 +0900

    Test를 위한 Like 쿼리 추가

--- src/test/java/com/ll/exam/sbb/SbbApplicationTest.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTest.java
@@ -53,4 +53,18 @@ class SbbApplicationTests {
         Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
         assertEquals(1, q.getId());
     }
+
+    @Test
+    void testJpa4() {
+        // SELECT * FROM question
+        Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1, q.getId());
+    }
+
+    @Test
+    void testJpa5() {
+        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
 }
