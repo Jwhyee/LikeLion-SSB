bfcab79995f674925fa4bfadae5f55e476b049e5
Jwhyee <kawnsdud@naver.com>
2022-08-10 14:09:08 +0900

    전체 Question을 가져오는 Test 추가

--- src/test/java/com/ll/exam/sbb/SbbApplicationTest.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTest.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SbbApplicationTest {
@@ -15,23 +17,35 @@ class SbbApplicationTest {
     private QuestionRepository questionRepository;
 
     @Test
+    void contextLoads() {
+    }
+
+    @Test
     void testJpa() {
         Question q1 = new Question();
         q1.setSubject("sbb가 무엇인가요?");
         q1.setContent("sbb에 대해서 알고 싶습니다.");
         q1.setCreateDate(LocalDateTime.now());
-        Question current = questionRepository.save(q1);  // 첫번째 질문 저장
-        assertEquals(current.getId(), 1);
+        questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q2);
+
+        assertThat(q1.getId()).isGreaterThan(0);
+        assertThat(q2.getId()).isGreaterThan(q1.getId());
     }
 
     @Test
-    void testGetEntity() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        Question current = questionRepository.save(q1);  // 첫번째 질문 저장
-        assertEquals(current.getId(), 1);
+    void testJpa2() {
+        List<Question> all = questionRepository.findAll();
+        assertEquals(2, all.size());
+
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+
     }
 
 
