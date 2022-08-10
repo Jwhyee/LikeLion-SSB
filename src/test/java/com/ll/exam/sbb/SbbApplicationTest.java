7be894995b1ef7ada7e978af2b596726284109a4
Jwhyee <kawnsdud@naver.com>
2022-08-10 13:48:26 +0900

    프로젝트 진행

---
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTest.java
@@ -0,0 +1,38 @@
+package com.ll.exam.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SbbApplicationTest {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    void testJpa() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        Question current = questionRepository.save(q1);  // 첫번째 질문 저장
+        assertEquals(current.getId(), 1);
+    }
+
+    @Test
+    void testGetEntity() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        Question current = questionRepository.save(q1);  // 첫번째 질문 저장
+        assertEquals(current.getId(), 1);
+    }
+
+
+}
