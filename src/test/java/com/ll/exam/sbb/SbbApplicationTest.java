a6d1c6567cad1e7d3384425c536e4d7dd17b27b0
Jwhyee <kawnsdud@naver.com>
2022-08-11 14:30:36 +0900

    test

--- src/test/java/com/ll/exam/sbb/SbbApplicationTest.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTest.java
@@ -1,5 +1,7 @@
 package com.ll.exam.sbb;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 class SbbApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
+
+    @BeforeEach
+    void test_list() {
+        truncate_Test();
+    }
 
     @Test
     void contextLoads() {
@@ -35,6 +42,125 @@ class SbbApplicationTests {
 
         // then
         assertEquals(q.getId(), 1);
+        questionRepository.truncateQuestion();
+    }
+
+    @Test
+    void save_Test() {
+        // given
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+
+        // when
+        Question q = questionRepository.save(q1);
+
+        // then
+        assertEquals(q.getId(), 1);
+    }
+
+    @Test
+    void delete_Test() {
+        // given
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        Question q = questionRepository.save(q1);
+
+        // when
+        questionRepository.delete(q);
+
+        // then
+        assertEquals(questionRepository.findAll().size(), 0);
+    }
+
+    @Test
+    void update_Test() {
+        // given
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        Question q = questionRepository.save(q1);
+
+        // when
+        q.setSubject("sbb가 무엇?");
+        q.setContent("sbb에 대해서 알고 싶");
+
+        // then
+        assertEquals(q.getSubject(), "sbb가 무엇?");
+    }
+
+    @Test
+    void findBySubject_Test() {
+        // given
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("sbb가 무엇");
+        q2.setContent("sbb에 대해서 알고 싶");
+        q2.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q2);
+
+        // when
+        Question fq1 = questionRepository.findBySubject("sbb가 무엇인가요?");
+        Question fq2 = questionRepository.findBySubject("sbb가 무엇");
+
+        // then
+        assertEquals(fq1.getId(), 1);
+        assertEquals(fq2.getId(), 2);
+    }
+
+    @Test
+    void findBySubjectAndContent_Test() {
+        // given
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("sbb가 무엇");
+        q2.setContent("sbb에 대해서 알고 싶");
+        q2.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q2);
+
+        // when
+        Question fq1 = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        Question fq2 = questionRepository.findBySubjectAndContent("sbb가 무엇", "sbb에 대해서 알고 싶");
+
+        // then
+        assertEquals(fq1.getId(), 1);
+        assertEquals(fq2.getId(), 2);
+    }
+
+    @Test
+    void findBySubjectLike_Test() {
+        // given
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("sbb가 무엇");
+        q2.setContent("sbb에 대해서 알고 싶");
+        q2.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q2);
+
+        // when
+        List<Question> fq = questionRepository.findBySubjectLike("%무엇%");
+
+        // then
+        assertEquals(fq.size(), 2);
     }
 
 }
