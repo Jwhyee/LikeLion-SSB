b54d95f2f36ac4cbc5398827613ecdc3b5a4256e
Jwhyee <kawnsdud@naver.com>
2022-08-11 13:28:57 +0900

    외래키를 살리고 truncate가 가능하도록 TEST 구현

--- src/main/java/com/ll/exam/sbb/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/QuestionRepository.java
@@ -1,9 +1,13 @@
 package com.ll.exam.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     Question findBySubject(String subject);
@@ -11,4 +15,9 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
 
     List<Question> findBySubjectLike(String target);
+
+    @Modifying
+    @Query(value = "TRUNCATE question;",
+            nativeQuery = true)
+    void truncateQuestion();
 }
--- src/test/java/com/ll/exam/sbb/SbbApplicationTest.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTest.java
@@ -1,5 +1,6 @@
 package com.ll.exam.sbb;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -76,6 +77,23 @@ class SbbApplicationTests {
         Question q = oq.get();
         q.setSubject("sbb가 뭔가요?");
         this.questionRepository.save(q);
+    }
+
+    @Test
+    @DisplayName("truncate_테스트")
+    void testJpa7() {
+        // given
+        questionRepository.truncateQuestion();
+
+        // when
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        Question q = questionRepository.save(q1);
+
+        // then
+        assertEquals(q.getId(), 1);
     }
 
 }
