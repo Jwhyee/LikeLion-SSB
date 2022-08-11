2fc781cc14d9ad09cd8893603fa20d0acab6c054
Jwhyee <kawnsdud@naver.com>
2022-08-11 13:31:22 +0900

    모든 Test Case를 지우고 다시 해보기

--- src/test/java/com/ll/exam/sbb/SbbApplicationTest.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTest.java
@@ -22,66 +22,7 @@ class SbbApplicationTests {
     }
 
     @Test
-    void testJpa() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        questionRepository.save(q1);
-
-        Question q2 = new Question();
-        q2.setSubject("스프링부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        questionRepository.save(q2);
-
-        assertThat(q1.getId()).isGreaterThan(0);
-        assertThat(q2.getId()).isGreaterThan(q1.getId());
-    }
-
-    @Test
-    void testJpa2() {
-        // SELECT * FROM question
-        List<Question> all = questionRepository.findAll();
-        assertEquals(2, all.size());
-
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
-    }
-
-    @Test
-    void testJpa3() {
-        // SELECT * FROM question
-        Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
-        assertEquals(1, q.getId());
-    }
-
-    @Test
-    void testJpa4() {
-        // SELECT * FROM question
-        Question q = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-        assertEquals(1, q.getId());
-    }
-
-    @Test
-    void testJpa5() {
-        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-        Question q = qList.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
-    }
-
-    @Test
-    void testJpa6() {
-        Optional<Question> oq = questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-        q.setSubject("sbb가 뭔가요?");
-        this.questionRepository.save(q);
-    }
-
-    @Test
-    @DisplayName("truncate_테스트")
-    void testJpa7() {
+    void truncate_Test() {
         // given
         questionRepository.truncateQuestion();
 
