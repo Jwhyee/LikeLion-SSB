92703224f34ec63e1bcb61cb3bcfc6757b545c3b
Jwhyee <kawnsdud@naver.com>
2022-08-12 13:36:35 +0900

    properties 추가

--- src/main/java/com/ll/exam/sbb/Question.java
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -1,13 +1,5 @@
 package com.ll.exam.sbb;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
-import java.time.LocalDateTime;
-import java.util.List;
-
 import lombok.Getter;
 import lombok.Setter;
 
